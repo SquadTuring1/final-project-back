4b53334a25e0e34294153c5e0b2f92c7d5e623e5
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-10-18 13:50:59 +0200

    fix remove api/ path from usersRoutes

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -5,9 +5,9 @@ const Router = express.Router;
 
 const UserRouter = Router();
 
-UserRouter.get("/api/users", UserControllerActions.getAllUsers);
-UserRouter.patch("/api/users/:uid", UserControllerActions.updateCurrentUser);
-UserRouter.get("/api/users/:uid", UserControllerActions.getCurrentUser);
-UserRouter.delete("/api/users/:id", UserControllerActions.deleteUser);
+UserRouter.get("/users", UserControllerActions.getAllUsers);
+UserRouter.patch("/users/:uid", UserControllerActions.updateCurrentUser);
+UserRouter.get("/users/:uid", UserControllerActions.getCurrentUser);
+UserRouter.delete("/users/:id", UserControllerActions.deleteUser);
 
 export default UserRouter;
