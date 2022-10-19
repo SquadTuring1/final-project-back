b67d896314d37ad807ba6298dc5398a75d0cf35b
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2022-10-19 18:29:31 +0200

    added cloudinaryId to song model

--- src/models/Song.js
+++ src/models/Song.js
@@ -1,43 +1,49 @@
 import mongoose from "mongoose";
 
 const SongSchema = new mongoose.Schema(
-    {
-        title: {
-            type:String,
-            trim: true,
-            required: [true, "Title is required"]
-        },
-        // fileUrl: {
-        //     type:String,
-        //     required: [true,"Url is required"],
-        //     unique: true
-        // },
-        thumbnail: {
-            type: String
-        },
-        released: {
-            type: Date,
-        },
-        duration: {
-            type:Number
-        },
-        album: [{
-            type: mongoose.Schema.ObjectId,
-            ref: "album" 
-        }],
-        genre: {
-            type: mongoose.Schema.ObjectId,
-            ref: "genre" 
-        },
-        likedBy: [{
-            type: mongoose.Schema.ObjectId,
-            ref: "user" 
-        }],
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Title is required"],
     },
-    {timestamps: true},
-)
+    fileUrl: {
+      type: String,
+      required: [true, "Url is required"],
+      unique: true,
+    },
+    thumbnail: {
+      type: String,
+    },
+    released: {
+      type: Date,
+    },
+    duration: {
+      type: Number,
+    },
+    cloudinaryId: {
+      type: String,
+    },
+    album: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "album",
+      },
+    ],
+    genre: {
+      type: mongoose.Schema.ObjectId,
+      ref: "genre",
+    },
+    likedBy: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
-const SongModel = new mongoose.model("song", SongSchema)
+const SongModel = new mongoose.model("song", SongSchema);
 
-export default SongModel
-
+export default SongModel;
