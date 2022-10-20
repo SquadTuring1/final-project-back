8939e5c3f0815e654339920b8508870d2a3c337e
Sefi Cohen <67965039+seficohen1@users.noreply.github.com>
2022-10-20 17:09:25 +0200

    .

--- src/config/config.js
+++ src/config/config.js
@@ -8,30 +8,30 @@ const ENV = process.env.NODE_ENV || "development";
 logger.enableAll();
 
 const CONFIG = {
-    production: {
-        // TODO:
+  production: {
+    // TODO:
+  },
+  development: {
+    app: {
+      PORT: process.env.PORT || 4000,
     },
-    development: {
-      app: {
-        PORT: process.env.PORT || 4000,
-      },
-      client: {
-        URL: process.env.CLIENT_URL || "http://localhost:3000",
-      },
-      logger: {
-        warn: logger.warn,
-        info: logger.info,
-        error: logger.error,
-        trace: logger.trace,
-        debug: logger.debug,
-      },
-      db: {
-        url: process.env.DB_URL,
-      },
+    client: {
+      URL: process.env.CLIENT_URL || "http://localhost:3000",
     },
-    test: {
-        // TODO:
+    logger: {
+      warn: logger.warn,
+      info: logger.info,
+      error: logger.error,
+      trace: logger.trace,
+      debug: logger.debug,
     },
-  };
-  
-  export default CONFIG;
+    db: {
+      url: process.env.DB_URL,
+    },
+  },
+  test: {
+    // TODO:
+  },
+};
+
+export default CONFIG;
